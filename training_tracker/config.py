c50f2fc7aadf09cbcd4cf4805711d60624fa40b8
Maryia Afanasyeva <masha.afanaseva0192@gmail.com>
2024-01-05 20:33:58 +0300

    created tables

---
+++ training_tracker/config.py
@@ -0,0 +1,41 @@
+from functools import lru_cache
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BaseConfig(BaseSettings):
+    ENV_STATE: Optional[str] = None
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+class GlobalConfig(BaseConfig):
+    ALGORITHM: Optional[str] = None
+    SECRET_KEY: Optional[str] = None
+    DATABASE_URL: Optional[str] = None
+    DB_FORCE_ROLL_BACK: bool = False
+
+
+class DevConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="DEV_")
+
+
+class ProdConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="PROD_")
+
+
+class TestConfig(GlobalConfig):
+    DATABASE_URL: str = "sqlite:///test.db"
+    DB_FORCE_ROLL_BACK: bool = True
+    SECRET_KEY: str = "fU0P9eJpl8qTOrypQZtN7BPbAoCyEiJ5"
+    ALGORITHM: str = "HS256"
+    model_config = SettingsConfigDict(env_prefix="TEST_")
+
+
+@lru_cache()
+def get_config(env_state: str):
+    configs = {"dev": DevConfig, "prod": ProdConfig, "test": TestConfig}
+    return configs[env_state]()
+
+
+config = get_config(BaseConfig().ENV_STATE)
---
+++ training_tracker/database.py
@@ -0,0 +1,128 @@
+import datetime
+
+import databases
+import sqlalchemy
+
+from training_tracker.config import config
+
+metadata = sqlalchemy.MetaData()
+
+distances = sqlalchemy.Table(
+    "distances",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("distance", sqlalchemy.Numeric),
+    sqlalchemy.Column("units", sqlalchemy.String),
+    sqlalchemy.Column(
+        "added_by_user_id",
+        sqlalchemy.ForeignKey("users.id", ondelete="SET NULL"),
+        nullable=False,
+    ),
+)
+
+groups = sqlalchemy.Table(
+    "groups",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column(
+        "added_by_user_id",
+        sqlalchemy.ForeignKey("users.id", ondelete="SET NULL"),
+        nullable=False,
+    ),
+)
+
+users = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("email", sqlalchemy.String, unique=True),
+    sqlalchemy.Column("password", sqlalchemy.String),
+    sqlalchemy.Column("confirmed", sqlalchemy.Boolean, default=False),
+    sqlalchemy.Column(
+        "created_at", sqlalchemy.DateTime, default=datetime.datetime.utcnow()
+    ),
+)
+
+weights = sqlalchemy.Table(
+    "weights",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("weight", sqlalchemy.Numeric),
+    sqlalchemy.Column("units", sqlalchemy.String),
+    sqlalchemy.Column(
+        "added_by_user_id",
+        sqlalchemy.ForeignKey("users.id", ondelete="SET NULL"),
+        nullable=False,
+    ),
+)
+
+exersices = sqlalchemy.Table(
+    "exersices",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column(
+        "group_id",
+        sqlalchemy.ForeignKey("groups.id", ondelete="SET NULL"),
+        nullable=False,
+    ),
+    sqlalchemy.Column(
+        "added_by_user_id",
+        sqlalchemy.ForeignKey("users.id", ondelete="SET NULL"),
+        nullable=False,
+    ),
+)
+
+trainings = sqlalchemy.Table(
+    "trainings",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column(
+        "training_date", sqlalchemy.DateTime, default=datetime.date.today()
+    ),
+    sqlalchemy.Column("status", sqlalchemy.String),
+    sqlalchemy.Column(
+        "user_id", sqlalchemy.ForeignKey("users.id", ondelete="SET NULL"), nullable=False
+    ),
+)
+
+sets = sqlalchemy.Table(
+    "sets",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("exercise_count", sqlalchemy.Integer),
+    sqlalchemy.Column(
+        "exercise_id",
+        sqlalchemy.ForeignKey("exersices.id", ondelete="CASCADE"),
+        nullable=False,
+    ),
+    sqlalchemy.Column(
+        "training_id",
+        sqlalchemy.ForeignKey("trainings.id", ondelete="CASCADE"),
+        nullable=False,
+    ),
+    sqlalchemy.Column(
+        "distance_id",
+        sqlalchemy.ForeignKey("distances.id", ondelete="CASCADE"),
+        nullable=True,
+    ),
+    sqlalchemy.Column(
+        "weight_id",
+        sqlalchemy.ForeignKey("weights.id", ondelete="CASCADE"),
+        nullable=True,
+    ),
+    sqlalchemy.Column(
+        "added_by_user_id",
+        sqlalchemy.ForeignKey("users.id", ondelete="SET NULL"),
+        nullable=False,
+    ),
+)
+
+engine = sqlalchemy.create_engine(config.DATABASE_URL)
+
+
+db_args = {"min_size": 1, "max_size": 3} if "postgres" in config.DATABASE_URL else {}
+database = databases.Database(
+    config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK, **db_args
+)
