fcff6295fcd39d0dc4f1be357936047c73c8c626
Maryia Afanasyeva <masha.afanaseva0192@gmail.com>
2024-01-10 15:05:18 +0300

    added tests for groups

--- training_tracker/tests/routers/test_groups.py
+++ training_tracker/tests/routers/test_groups.py
@@ -1,5 +1,12 @@
 import pytest
 from httpx import AsyncClient
+from training_tracker.tests.helpers import create_group
+from training_tracker import security
+
+
+@pytest.fixture()
+async def created_group(async_client: AsyncClient, logged_in_token: str):
+    return await create_group("Test group", async_client, logged_in_token)
 
 
 @pytest.mark.anyio
@@ -12,9 +19,39 @@ async def test_create_group(
         json={"name": name},
         headers={"Authorization": f"Bearer {logged_in_token}"},
     )
-    # assert response.status_code == 201
+    assert response.status_code == 201
     assert {
         "id": 1,
         "name": name,
         "added_by_user_id": confirmed_user["id"],
     }.items() <= response.json().items()
+    
+
+@pytest.mark.anyio
+async def test_create_group_wrong_data(async_client: AsyncClient, logged_in_token: str):
+    response = await async_client.post(
+        "/group",
+        json={},
+        headers={"Authorization": f"Bearer {logged_in_token}"},
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.anyio
+async def test_create_group_expired_token(async_client: AsyncClient, mocker, confirmed_user: dict):
+    mocker.patch("training_tracker.security.access_token_expiry_minutes", return_value=-1)
+    token = security.create_access_token(confirmed_user["email"])
+    response = await async_client.post(
+        "/group",
+        json={"name": "Test group"},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 401
+    assert "Token has expired" in response.json()["detail"]
+
+
+@pytest.mark.anyio
+async def test_get_all_groups(async_client: AsyncClient, created_group: dict):
+    response = await async_client.get("/groups")
+    assert response.status_code == 200
+    assert created_group.items() <= response.json()[0].items()
